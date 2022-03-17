8fa58ac8aa6585c7c3da660cab203305fa5e89a7
satoru0725 <ap03095@icloud.com>
2022-03-17 15:21:38 +0900

    FactoryBot記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,19 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name }
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    #password              {Faker::Internet.password(min_length: 6)}
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
     password_confirmation {password}
+
+    transient do
+      person { Gimei.name }      
+    end
+    last_name { person.last.kanji }
+    first_name { person.first.kanji }
+    last_name { person.last.katakana }
+    first_name { person.first.katakana }
+
+    birth_date {Faker::Date.birthday}
+    
   end
 end
