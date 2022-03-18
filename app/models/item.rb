7f82212fcca18e3be36e9e1d5b90c3f67b46c329
satoru0725 <ap03095@icloud.com>
2022-03-18 16:32:37 +0900

    ActiveHashでcategory項目追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one_attached :image
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :price, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "Can't be blank" }
 end
