8fec3ee4dc5332377f0acce5785a1346d98c7cac
satoru0725 <ap03095@icloud.com>
2022-03-18 17:30:26 +0900

    ActiveHashでshipping_fee_status項目追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,10 +4,12 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :category
   belongs_to :sales_status
+  belongs_to :shipping_fee_status
 
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
   validates :category_id, numericality: { other_than: 1, message: "Can't be blank" }
   validates :sales_status_id, numericality: { other_than: 1, message: "Can't be blank"}
+  validates :shipping_fee_status_id, numericality: { other_than: 1, message: "Can't be blank"}
 end
