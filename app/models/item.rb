82df993d9f10dba30033ba16c9f5720767e94250
satoru0725 <ap03095@icloud.com>
2022-03-18 17:19:26 +0900

    ActiveHashでsales_status項目追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,9 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   belongs_to :category
+  belongs_to :sales_status
 
   validates :name, presence: true
   validates :info, presence: true
   validates :price, presence: true
   validates :category_id, numericality: { other_than: 1, message: "Can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1, message: "Can't be blank"}
 end
