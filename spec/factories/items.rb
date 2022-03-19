4e77f8259d432f8cf9888c63ddf29e23c56755da
satoru0725 <ap03095@icloud.com>
2022-03-19 17:18:41 +0900

    FactoryBot定義

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
+    image                   { Faker::Lorem.sentence }
+    name                    { Faker::Commerce.product_name }
+    info                    { Faker::Lorem.sentence }
+    price                   { Faker::Number.between(from: 300, to: 9999999) }
+    category_id             { Faker::Number.between(from: 2, to: 11) }
+    sales_status_id         { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_status_id  { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id           { Faker::Number.between(from: 2, to: 48) }
+    schedule_delivery_id    { Faker::Number.between(from: 2, to: 4) }
     
   end
 end
