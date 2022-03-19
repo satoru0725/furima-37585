49290708f3f6350c134178bb8b16cd980f7e976c
satoru0725 <ap03095@icloud.com>
2022-03-19 18:09:20 +0900

    FactoryBotのimage生成の修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    image                   { Faker::Lorem.sentence }
     name                    { Faker::Commerce.product_name }
     info                    { Faker::Lorem.sentence }
     price                   { Faker::Number.between(from: 300, to: 9999999) }
@@ -9,6 +8,10 @@ FactoryBot.define do
     shipping_fee_status_id  { Faker::Number.between(from: 2, to: 3) }
     prefecture_id           { Faker::Number.between(from: 2, to: 48) }
     schedule_delivery_id    { Faker::Number.between(from: 2, to: 4) }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品情報の保存' do
+    context '商品情報が投稿できる場合' do
+      it '9項目全てを記述すると投稿できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品情報が投稿できない場合' do
+      it '商品画像が空では投稿できない' do
+      end
+      it '商品名が空では投稿できない' do
+      end
+      it '商品の説明が空では投稿できない' do
+      end
+      it 'カテゴリーの情報が未選択だと投稿できない' do
+      end
+      it '商品の状態の情報が未選択だと投稿できない' do
+      end
+      it '商品の状態の情報が未選択だと投稿できない' do
+      end
+      it '配送料の負担の情報が未選択だと投稿できない' do
+      end
+      it '発送元の地域の情報が未選択だと投稿できない' do
+      end
+      it '発送までの日数の情報が未選択だと投稿できない' do
+      end
+      it '価格の情報が未選択だと投稿できない' do
+      end
+      it '価格の数値が¥300未満だと投稿できない' do
+      end
+      it '価格の数値が¥9999999より大きいと投稿できない' do
+      end
+      it '価格の情報が全角だと投稿できない' do
+      end
+      it '価格の情報が半角英字だと投稿できない' do
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+      end
+    end
+  end
 end
