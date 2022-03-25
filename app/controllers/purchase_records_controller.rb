3114549dbc0b8fc02d3db62fd8ca8a5d4abac670
satoru0725 <ap03095@icloud.com>
2022-03-25 16:25:00 +0900

    フォーム情報の保存確認とエラー表示の実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,9 +1,27 @@
 class PurchaseRecordsController < ApplicationController
+  before_action :set_item, only: [:index, :create]
   
   def index
     @purchase_address =PurchaseAddress.new
   end
 
   def create
+    @purchase_address = PurchaseAddress.new(purchase_params)
+    if @purchase_address.valid?
+       @purchase_address.save
+       redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+  #:number,:exp_month,:exp_year,cvcのカード情報は保存確認のためpermitを通すために一時的に記述。後に削除すること。
+  def purchase_params
+    params.require(:purchase_address).permit( :number, :exp_month, :exp_year, :cvc, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,20 +1,19 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :purchase_record
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :addresses, :building, :phone_number, :number, :exp_month, :exp_year, :cvc
   
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
     validates :city
     validates :addresses
     validates :phone_number, numericality: { only_integer: true }, length: { in:10..11}
-    validates :purchase_record_id
   end
 
   def save
-    purchase = PurchaseRecord.create(uer_id: user_id, item_id: item_id)
+    purchase = PurchaseRecord.create(user_id: user_id, item_id: item_id)
     DeliveryAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, addresses: addresses, phone_number: phone_number, purchase_record_id: purchase.id)
   end
 end
