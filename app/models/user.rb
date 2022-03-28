57c1ef913500e41fce22407b2160da3668c5ba17
satoru0725 <ap03095@icloud.com>
2022-03-28 14:44:08 +0900

    モデル、ビュー、テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  has_many :purchase_record
+  has_many :purchase_records
 
   validates :nickname, presence: true
 
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe PurchaseAddress, type: :model do
   describe '配送先情報の保存' do
     before do
+      user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
-      @purchase_address = FactoryBot.build(:purchase_address, user_id: item.user.id, item_id: item.id)
+      @purchase_address = FactoryBot.build(:purchase_address, user_id: user.id, item_id: item.id)
       sleep 0.1
     end
 
