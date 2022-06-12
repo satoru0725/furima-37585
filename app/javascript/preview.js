8e8e384324098adde8ce9acf8a12768cfc55203e
satoru0725 <ap03095@icloud.com>
2022-06-12 18:48:57 +0900

    preview.jsの読み込み確認

---
+++ app/javascript/preview.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+  const postForm = document.getElementById('new_post');
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
