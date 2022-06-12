1ff9c5d38aba98e6136fbe67029173ae1c579dc8
satoru0725 <ap03095@icloud.com>
2022-06-12 19:01:47 +0900

    input要素の変化での発火を確認

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
+  const previewList = document.getElementById('previews');
   if (!postForm) return null;
   console.log("preview.jsが読み込まれました");
+
+  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  fileField.addEventListener('change', function(e){
+    console.log("input要素で値の変化が起きました");
+  });
 });
