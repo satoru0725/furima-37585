ad539630d1c7bb57a82276be7361753c7502b019
satoru0725 <ap03095@icloud.com>
2022-06-12 19:27:00 +0900

    コードのリファクタリング

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function(){
   const postForm = document.getElementById('new_post');
   const previewList = document.getElementById('previews');
   if (!postForm) return null;
-  console.log("preview.jsが読み込まれました");
 
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   fileField.addEventListener('change', function(e){
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function(){
     if (alreadyPreview) {
       alreadyPreview.remove();
     };
-    console.log("input要素で値の変化が起きました");
     console.log(e.target.files[0]);
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
