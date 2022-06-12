16743ca52c5db7b712eb7cd43d588308c8b896e1
satoru0725 <ap03095@icloud.com>
2022-06-12 19:17:18 +0900

    プレビュー画像表示の確認

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -7,5 +7,17 @@ document.addEventListener('DOMContentLoaded', function(){
   const fileField = document.querySelector('input[type="file"][name="item[image]"]');
   fileField.addEventListener('change', function(e){
     console.log("input要素で値の変化が起きました");
+    console.log(e.target.files[0]);
+    const file = e.target.files[0];
+    const blob = window.URL.createObjectURL(file);
+    console.log(blob);
+    const previewWrapper = document.createElement('div');
+    previewWrapper.setAttribute('class', 'preview');
+    const previewImage = document.createElement('img');
+    previewImage.setAttribute('class', 'preview-image');
+    previewImage.setAttribute('src', blob);
+
+    previewWrapper.appendChild(previewImage);
+    previewList.appendChild(previewWrapper);
   });
 });
