32e0197339594c67272327f209c71437e078ea54
satoru0725 <ap03095@icloud.com>
2022-06-15 07:51:30 +0900

    2つ目のファイル選択ボタンの表示

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
   const previewList = document.getElementById('previews');
   if (!postForm) return null;
 
-  const fileField = document.querySelector('input[type="file"][name="item[image]"]');
+  const fileField = document.querySelector('input[type="file"][name="item[images][]"]');
   fileField.addEventListener('change', function(e){
     const alreadyPreview = document.querySelector('.preview');
     if (alreadyPreview) {
@@ -17,11 +17,18 @@ document.addEventListener('DOMContentLoaded', function(){
     previewWrapper.setAttribute('class', 'preview');
     const previewImage = document.createElement('img');
     previewImage.setAttribute('class', 'preview-image');
-    previewImage.setAttribute('width', '400');
-    previewImage.setAttribute('height', '260');
+    //previewImage.setAttribute('width', '400');
+    //previewImage.setAttribute('height', '260');
     previewImage.setAttribute('src', blob);
 
     previewWrapper.appendChild(previewImage);
     previewList.appendChild(previewWrapper);
+
+    const newFileField = document.createElement('input');
+    newFileField.setAttribute('type', 'file');
+    newFileField.setAttribute('name', 'post[images][]');
+
+    const fileFieldsArea = document.querySelector('.click-upload');
+    fileFieldsArea.appendChild(newFileField);
   });
 });
