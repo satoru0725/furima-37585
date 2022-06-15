d823a2b6ca35f77cab5b06f4d4774b0002f6ad5d
satoru0725 <ap03095@icloud.com>
2022-06-15 09:40:55 +0900

    画像の差し替え

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -33,6 +33,9 @@ document.addEventListener('DOMContentLoaded', function(){
     const nextDataIndex = Number(lastFileField.getAttribute('data-index')) + 1;
     newFileField.setAttribute('data-index', nextDataIndex);
 
+    //追加されたfile_fieldにchangeイベントをセット
+    newFileField.addEventListener("change", changeFileField);
+
     //生成したfile_fieldを表示
     const fileFieldsArea = document.querySelector('.click-upload');
     fileFieldsArea.appendChild(newFileField);
@@ -44,12 +47,23 @@ document.addEventListener('DOMContentLoaded', function(){
     const dataIndex = e.target.getAttribute('data-index');
 
     //古いプレビューが存在する場合は削除
-    const alreadyPreview = document.querySelector('.preview');
+    /*const alreadyPreview = document.querySelector('.preview');
     if (alreadyPreview) {
       alreadyPreview.remove();
     };
+    */
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
+
+    //data-indexを使用して、すでにプレビューが表示されているかを確認する
+    const alreadyPreview = document.querySelector('.preview[data-index="${dataIndex}"]');
+
+    if (alreadyPreview) {
+      //クリックしたfile_fieldのdata-indexと、同じ番号のプレビュー画像がすでに表示されている場合は、画像の差し替えのみを行う
+      const alreadyPreviewImage = alreadyPreview.querySelector("img");
+      alreadyPreviewImage.setAttribute("src", blob);
+      return null;
+    };
 
     buildPreviewImage(dataIndex, blob);
     buildNewFileField();
